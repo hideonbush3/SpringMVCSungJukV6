96fd37a30f9e4f5df50aca6a74ca5a9b5456172e
hideonbush3 <ppoii0961@gmail.com>
2023-04-25 15:50:39 +0900

    스프링MVC 프로젝트 - 성적 조회2

---
+++ src/main/java/hideonbush3/spring4/sungjukv6/controller/SungJukController.java
@@ -0,0 +1,29 @@
+package hideonbush3.spring4.sungjukv6.controller;
+
+import hideonbush3.spring4.sungjukv6.service.SungJukV6Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SungJukController {
+
+    private SungJukV6Service sjsrv;
+
+    @Autowired
+    public SungJukController(SungJukV6Service sjsrv) {
+        this.sjsrv = sjsrv;
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model){
+        System.out.println(sjsrv.readSungJuk());
+
+        // sungjuklist.jsp에 성적조회결과를 sjs라는 이름으로 넘김
+        model.addAttribute("sjs", sjsrv.readSungJuk());
+        return "sungjuklist";
+    }
+}
