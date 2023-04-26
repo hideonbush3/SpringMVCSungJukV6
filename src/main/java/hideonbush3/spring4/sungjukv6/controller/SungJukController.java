a1ed2754712082838f361b562cb9c46d59017124
hideonbush3 <ppoii0961@gmail.com>
2023-04-26 13:07:04 +0900

    스프링MVC 프로젝트 - 성적 저장 2

--- src/main/java/hideonbush3/spring4/sungjukv6/controller/SungJukController.java
+++ src/main/java/hideonbush3/spring4/sungjukv6/controller/SungJukController.java
@@ -1,13 +1,11 @@
 package hideonbush3.spring4.sungjukv6.controller;
 
+import hideonbush3.spring4.sungjukv6.model.SungJukVO;
 import hideonbush3.spring4.sungjukv6.service.SungJukV6Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,6 +18,7 @@ public class SungJukController {
         this.sjsrv = sjsrv;
     }
 
+    // 성적 리스트 처리
     @GetMapping(value = "/list")
     public ModelAndView list(){
         ModelAndView mv = new ModelAndView();
@@ -27,6 +26,24 @@ public class SungJukController {
         // sungjuklist.jsp에 성적조회결과를 sjs라는 이름으로 넘김
         mv.addObject("sjs", sjsrv.readSungJuk());
         mv.setViewName("sungjuklist");  // 뷰 이름 지정
+
+        return mv;
+    }
+
+    // 성적 입력폼 표시
+    @GetMapping(value = "/add")
+    public String add(){
+
+        return "sungjuk";
+    }
+
+    // 성적 입력 처리
+    @PostMapping(value = "/add")
+    public ModelAndView addok(SungJukVO sj){
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("sj", sj);
+        mv.setViewName("sungjukok");
 
         return mv;
     }
---
+++ src/main/webapp/WEB-INF/views/sungjuk.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>성적처리 프로그램 v6</title>
+</head>
+<body>
+<h1>성적처리 프로그램 v6</h1>
+
+<!-- action 속성을 포함하지 않으면 submit 했을때 자기 자신을 다시 부름 -->
+<form name="sj" method="post">
+    <div>이름 : <input type = "text" name = "name"></div>
+    <div>국어 : <input type = "text" name = "kor"></div>
+    <div>영어 : <input type = "text" name = "eng"></div>
+    <div>수학 : <input type = "text" name = "mat"></div>
+    <div><button type="submit">입력완료</button></div>
+</form>
+</body>
+</html>
